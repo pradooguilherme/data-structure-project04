ee47e72d978f21bcfd7cf3d10f1807ad0cd49835
Guilherme Narciso do Prado <guipradu2020@gmail.com>
2024-11-12 08:28:26 -0300

    Insertion Functions done

--- main.c
+++ main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
+
+#define TAM_HASH 13
 
 typedef struct Register
 {
@@ -15,9 +18,8 @@ typedef struct Register
 
 typedef struct Key
 {
-    char id_aluno[4];
-    char sigla_disc[4];
-
+    char key[2][7];
+    int rrn[2];
 } Key;
 
 Register *read_insert_file()
@@ -98,6 +100,38 @@ FILE *iniciaArquivo()
     return seuArquivo;
 }
 
+FILE *iniciaHash()
+{
+
+    FILE *hash_file = fopen("hash.bin", "r+b");
+
+    if (hash_file == NULL)
+    {
+        hash_file = fopen("hash.bin", "w+b");
+
+        if (hash_file != NULL)
+        {
+
+            char no_key = '#';
+
+            for (int i = 0; i < TAM_HASH; i++)
+            {
+                fseek(hash_file, i * sizeof(Key), SEEK_SET);
+                fwrite(&no_key, sizeof(char), 1, hash_file);
+                fseek(hash_file, 6, SEEK_CUR);
+                fwrite(&no_key, sizeof(char), 1, hash_file);
+            }
+        }
+        else
+        {
+            printf("Falha na abertura do arquivo hash.\n");
+            return NULL;
+        }
+    }
+
+    return hash_file;
+}
+
 int getLastRecordInserted()
 {
 
@@ -138,6 +172,22 @@ void writeOperationFlag(FILE *data_file)
     return;
 }
 
+int calcula_tamanho(Register *registro)
+{
+
+    int tam = 0;
+
+    tam += strlen(registro->id_aluno);
+    tam += strlen(registro->sigla_disc);
+    tam += strlen(registro->nome_aluno);
+    tam += strlen(registro->nome_disc);
+    tam += sizeof(registro->media);
+    tam += sizeof(registro->freq);
+    tam = tam + 5;
+
+    return tam;
+}
+
 void insertRegister(Register *registro)
 {
     FILE *data_file = iniciaArquivo();
@@ -176,6 +226,120 @@ void insertRegister(Register *registro)
     fclose(data_file);
 }
 
-int main(){
-    return 1;
+int hash(char key[7], int overflow)
+{
+    int chave = atoi(key);
+    return (chave + overflow) % TAM_HASH;
+}
+
+Key *getKeysOnAdress(FILE *hash, int address)
+{
+
+    Key *registro = (Key *)malloc(sizeof(Key));
+
+    if (registro == NULL)
+    {
+        printf("Falha na alocação de memória.\n");
+        return NULL;
+    }
+
+    fseek(hash, address, SEEK_SET);
+    fread(registro, sizeof(Key), 1, hash);
+
+    return registro;
+}
+
+int avaiblePosition(Key *registro)
+{
+
+    if (strcmp(registro->key[0], "#") == 0)
+    {
+        return 0;
+    }
+
+    if (strcmp(registro->key[1], "#") == 0)
+    {
+        return 1;
+    }
+
+    return -1;
+}
+
+int lookForPosition(FILE *hash_file, char key[7], int *address)
+{
+    int realAddress = hash(key, 0);
+    int lastAddress = realAddress;
+
+    Key *registro = getKeysOnAdress(hash_file, realAddress);
+
+    int pos = avaiblePosition(registro);
+
+    if (pos != -1)
+    {
+        free(registro);
+        return pos;
+    }
+
+    free(registro);
+
+    int i = 1;
+
+    printf("Aviso: Houve colisão.\n");
+
+    while (1)
+    {
+        printf("Aviso: Tentativa %d.\n", i);
+
+        realAddress = hash(key, i);
+
+        if (realAddress == lastAddress)
+        {
+            return -1;
+        }
+
+        registro = getKeysOnAdress(hash_file, realAddress);
+
+        pos = avaiblePosition(registro);
+        if (pos != -1)
+        {
+            free(registro);
+            *address = realAddress;
+            return pos;
+        }
+
+        free(registro);
+        i++;
+    }
+}
+
+void insereHash(FILE *hash_file, char key[7], int rrn)
+{
+    int address;
+    int pos = lookForPosition(hash_file, key, &address);
+
+    if (pos == -1)
+    {
+        printf("A hash está cheia.\n");
+        return;
+    }
+
+    Key *keyOnAdress = getKeysOnAdress(hash_file, address);
+
+    strcpy(keyOnAdress->key[pos], key);
+    keyOnAdress->rrn[pos] = rrn;
+
+    fseek(hash_file, address, hash_file);
+    fwrite(keyOnAdress, sizeof(Key), 1, hash_file);
+
+    printf("Chave %s inserida com sucesso no endereço %d.\n", key, address);
+
+    free(keyOnAdress);
+    return;
+}
+
+int main()
+{
+    FILE *hash = iniciaHash();
+
+    fclose(hash);
 }
